7390a0e71932c4e518064ac4c50522cc0a609bc9
fr1sk <fr1sk@live.com>
2019-10-26 23:50:19 +0200

    :robot: WIP - cron

--- backend/src/lib/utils.ts
+++ backend/src/lib/utils.ts
@@ -1,11 +1,29 @@
-import * as fs from 'fs';
+// import * as fs from 'fs';
 
-async function addDataToJson(): Promise<Response> {
-  const data: any = fs.readFileSync('../../atm.json');
-  const key: string = 'ISO-8859-2';
-  console.log('🗽', JSON.parse(data, key));
-}
+// function addDataToJson(): void {
+//   const data: any = fs.readFileSync('../../atm.json');
+//   const key: string = 'ISO-8859-2';
+//   console.log('🗽', JSON.parse(data, key));
+// }
 
-(async () => {
-  await addDataToJson();
-})();
+// (async () => {
+//   await addDataToJson();
+// })();
+
+import { cronJob } from 'cron';
+
+const checkCron = new cronJob('5 * * * * *', (async () => {
+  try {
+    console.log('⏲ cron checker');
+    const allTransactions = await Cards.find({ 'config.wheel_card.limit': 'WEEKLY'});
+    await Promise.all(allWeeklyWheelCard.map(async x => {
+      await WheelUsage.deleteMany({ card: x._id });
+    }));
+  } catch (e) {
+    console.log('⌚🛑 Error in wheelWeeklyCron', e);
+  }
+}), null, false, 'America/Los_Angeles');
+
+
+
+
