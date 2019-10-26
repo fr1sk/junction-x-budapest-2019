1dc96eac0c5260bd459173900bc201e1d7d4266e
Branko Gvoka <bane92ru@gmail.com>
2019-10-26 21:20:05 +0200

    WIP

--- backend/src/domain/use_cases/undoReservation.ts
+++ backend/src/domain/use_cases/undoReservation.ts
@@ -1,5 +1,20 @@
-export async function undoReservation(){
+import {CurrencyType} from "root/src/api/routes/transaction/types";
+import {atmRepository, transactionRepository} from "root/src/gateways";
+
+export async function undoReservation(atm_id: string, currency: CurrencyType, transaction_id: string){
+
+    const transaction = await transactionRepository.findOneById(transaction_id);
+    transactionRepository.updateTransaction(transaction_id, {
+        used: true
+    });
+
+    // const atm = await atmRepository.getAtm(atm_id);
+    // const balance = atm.balance - transaction.;
+    // atmRepository.updateAtm(atm_id, {
+    //
+    // })
+
+
+    /// res
 //
-
-
 }
