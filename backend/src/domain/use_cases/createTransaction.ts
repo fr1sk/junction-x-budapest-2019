2c9a92a9974c4338cbc14a8d9413c2ced9a3af18
zsevic <zsevic95@gmail.com>
2019-10-27 02:15:13 +0200

    :bug: Fix types

--- backend/src/domain/use_cases/createTransaction.ts
+++ backend/src/domain/use_cases/createTransaction.ts
@@ -1,14 +1,14 @@
-import { TransactionRequest } from 'api/routes/transaction/types';
+import { TransactionRequest, TransactionResponse } from 'api/routes/transaction/types';
 import { encrypt } from 'lib/encryption';
-import {atmRepository, transactionRepository} from 'gateways';
-import moment, { Moment } from 'moment';
+import { atmRepository, transactionRepository } from 'gateways';
+import moment from 'moment';
 import { TransactionType } from 'domain/entities/Transaction';
 
 export async function createTransaction(
   {
     CURRENCY, AMOUNT, ATM_ID, USER_ID, EST_TIME_IN_MINS,
   }: TransactionRequest,
-): Promise<{qr_code: string, valid_until: Moment}> {
+): Promise<TransactionResponse> {
 
   const VALID_UNTIL = moment().utc().add(EST_TIME_IN_MINS, 'minutes');
   const data = {CURRENCY, AMOUNT};
@@ -28,7 +28,7 @@ export async function createTransaction(
   await atmRepository.decrementBalance(ATM_ID, transaction.currency_type, transaction.amount);
   try {
     await transactionRepository.createTransaction(transaction);
-    return {qr_code: QR_CODE, valid_until: VALID_UNTIL};
+    return { QR_CODE, VALID_UNTIL};
   } catch(err){
     await atmRepository.incrementBalance(ATM_ID, transaction.currency_type, transaction.amount);
     throw new Error('Transaction failed')
