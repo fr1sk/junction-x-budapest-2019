ccc5222ea2757741612978326769457ef4e82572
Branko Gvoka <bane92ru@gmail.com>
2019-10-27 02:59:06 +0100

    :sprinkle: Add qr code preview - view

---
+++ frontend/src/app/modules/qr-code-preview/qr-code-preview.routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import {QrCodeComponent} from "./qr-code/qr-code.component";
+
+const routes: Routes = [
+    {
+        path: '',
+        component: QrCodeComponent
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class QrCodePreviewRoutingModule {}
