fd369a617ecc24849d999fa59f5d716005860ced
fr1sk <fr1sk@live.com>
2019-10-27 10:54:41 +0100

    :robot: sorted distances by matrix api results

--- stress-tester/index.js
+++ stress-tester/index.js
@@ -9,7 +9,7 @@ async function main() {
     const long = 19.0790687;
 
     const recommendations = await Promise.all(
-        _.range(0, 20)
+        _.range(0, 1)
             .map(async i => {
                 const request = {
                     location: {
@@ -25,7 +25,7 @@ async function main() {
                 }
                 // console.log(`req: ${i} - ${JSON.stringify(request)}`);
                 const r = await axios.post(`${backendUrl}/api/atms/recommend`, request);
-                // console.log(r.data[0]);
+                console.log(r.data);
                 return r.data[0];
             })
     );
