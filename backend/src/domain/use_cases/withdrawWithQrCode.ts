1102de5683dee986d69958543a279992cf47c38c
Branko Gvoka <bane92ru@gmail.com>
2019-10-27 02:47:10 +0200

    :rocket: Withdraw works!

--- backend/src/domain/use_cases/withdrawWithQrCode.ts
+++ backend/src/domain/use_cases/withdrawWithQrCode.ts
@@ -1,15 +1,19 @@
 import { transactionRepository, userRepository } from 'gateways';
 import { decrypt } from 'lib/encryption';
-import moment = require("root/node_modules/moment");
+import moment from 'moment';
+import {Transaction} from "domain/entities";
 
-export async function withdrawWithQrCode(transaction_id: string, qr_code: string): Promise<void> {
-    const qr_code_data = JSON.parse(decrypt(qr_code)); // CURRENCY + AMOUNT
+export async function withdrawWithQrCode(transaction_id: string, qr_code: string): Promise<Transaction> {
+
+    JSON.parse(decrypt(qr_code)); // CURRENCY + AMOUNT
+
     const transaction = await transactionRepository.findByReservationIdAndQrCode(transaction_id, qr_code);
     const now = moment();
+
     const valid_until = moment(transaction.valid_until);
     if (now.isBefore(valid_until) && transaction.type === 'RESERVE') {
         await userRepository.decrementBalance(transaction.user, transaction.amount);
-        await transactionRepository.updateTransaction(transaction_id, {type: 'WITHDRAW'});
+        return transactionRepository.updateTransaction(transaction_id, {type: 'WITHDRAW'});
     } else {
         // rollback..
         throw new Error('QR code has expired or already used.');
